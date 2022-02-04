e3db3e5655db28962f59f73079c296a23d6139c1
Weiyan Zhu <weiyanz@berkeley.edu>
2022-02-03 18:20:08 -0800

    link to front end

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(express.static(`./public`));
 app.use("/api/v1/tasks", taskRouter);
 
 const startServer = async () => {
